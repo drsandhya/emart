648e54ec616daa14467bbaabe8e059ae2208e36d
sandhya <sindhusandhya22@gmail.com>
2021-12-29 02:37:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./component/Home";
+import Navbar from "./component/Navbar";
+import Products from "./component/Products";
+import Product from "./component/Product";
+import { Routes, Route} from "react-router-dom";
+import Cart from "./component/Cart";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<Product />} />
+        <Route path="cart" element={<Cart/ >} />
+      </Routes>
+   
+    </>
   );
 }
 
 export default App;
+
---
+++ src/redux/action/index.jsx
@@ -0,0 +1,17 @@
+// item add in cart
+
+export const addCart = (product) => {
+return{
+  type : "ADDITEM",
+  payload : product
+ }
+}
+
+export const delCart = (product) => {
+  return{
+    type : "DELITEM",
+    payload : product
+  }
+
+}
+ 
